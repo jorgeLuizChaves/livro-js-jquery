68a854add90c9b9c2030fd49819a661d9c5c7c13
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2014-01-09 00:49:48 -0200

    created function that convert a value into a string money formatted valueToMoneyTest

--- JSLab/javascripts/rogus.js
+++ JSLab/javascripts/rogus.js
@@ -2,3 +2,11 @@ function convertTextToFloat(text){
     var floatValue = text.replace("R$", "").replace(",",".");
     return parseFloat(floatValue);
 }
+
+function floatToMoneyText(value){
+    const COMMA = ",";
+    value = value * 100;
+    var stringValue = value.toString();
+    var result = "R$" + stringValue.substr(0, stringValue.length - 2) + COMMA + stringValue.substr(stringValue.toString().length - 2);
+   return result;
+}
