b1c097a0e604132870897e44e9d7b5e5d3e2aa07
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2014-01-12 18:02:15 -0200

    refactored to use jQuery

--- JSLab/javascripts/rogus.js
+++ JSLab/javascripts/rogus.js
@@ -7,63 +7,40 @@ function floatToMoneyText(value){
     const COMMA = ",";
     value = Math.floor(value * 100);
     var stringValue = (value < 1 ? "1" : value.toString());
-    var result = "R$ " + stringValue.substr(0, stringValue.length - 2) + COMMA + stringValue.substr(stringValue.toString().length - 2);
+    var result = "R$ " + stringValue.substr(0, stringValue.length - 2)
+    + COMMA + stringValue.substr(stringValue.toString().length - 2);
    return result;
 }
 
 function readTotal(){
-    var moneyTotal = document.getElementById("total").innerHTML;
-    return floatToMoneyText(moneyTotal);
+    return floatToMoneyText($("#total").text());
 }
 
 function writeTotal(value){
-    document.getElementById("total").innerHTML = value;
+    $("#total").text(value);
 }
 
 function calculateTotalProducts(){
 
-
-    hasSupportToClassName();
-
-
-    var products = document.getElementsByClassName("produto");
+    var $products = $(".produto");
 
     var total =0;
-    for(var cont=0; cont < products.length; cont++){
-        var quantityElements =   products[cont].getElementsByClassName("quantity");
-        var priceElements = products[cont].getElementsByClassName("price");
+    $products.each(function(pos, produto){
+        var quantityElements =   $(produto).find(".quantity").val();
+        var priceElements = $(produto).find(".price").text();
 
-        var quantity = convertTextToFloat(quantityElements[0].value);
-        var price = convertTextToFloat(priceElements[0].innerHTML);
+        var quantity = convertTextToFloat(quantityElements);
+        var price = convertTextToFloat(priceElements);
 
         var subTotal = quantity * price;
         total += subTotal;
-    }
+    })
+
     return floatToMoneyText(total);
 }
 
 function quantityModified(){
     writeTotal(calculateTotalProducts());
-}
-
-
-
-function hasSupportToClassName(){
-    if("undefined" === "undefined"){
-        document.getElementsByClassName = function(className){
-            var allElements = document.getElementsByTagName("*");
-            var result = [];
-            var element;
-
-            for(var cont=0; (element = allElements[cont]) != null; cont++){
-                if(element.className && element.className.indexOf(className) != -1){
-                    console.log(element.className.indexOf(className));
-                    result.push(element);
-                }
-            }
-            return result;
-        }
-    }
 }
 
 window.onload = function(){
