44eaa25206e6d142550991d87e3f2b3a3a9e1f07
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2014-01-18 19:35:34 -0200

    todo app working

--- JSLab/javascripts/todo.js
+++ JSLab/javascripts/todo.js
@@ -1,105 +1,79 @@
 $(function(){
-    $lastClicked = false;
-    function onTarefaEditClick(event){
-        if(event.which === 13){
-            var tarefaEditada = $(this).find(".tarefa-edit").val();
+    const ENTER_CLICK = 13;
+    var $lastClicked;
 
-            if(tarefaEditada !== null && tarefaEditada !== ""){
-                $(this).off();
-                $(this).parent(".tarefa-edit-item");
-
-                $(this).children().each(function(i, div){
-                    $(div).remove();
-                });
-                $(this).empty();
-                $(this).append("<div class='tarefa-texto'>" + tarefaEditada + "</div>")
-                    .append("<div class='tarefa-delete'></div>")
-                    .append("<div class='clear'></div>");
-
-                $(this).removeClass("tarefa-edit-item");
-                $(this).addClass("tarefa-item");
-                $lastClicked = false;
-
-                $(".tarefa-delete").click(onTarefaDelete);
-                $(".tarefa-item").click(onTarefaItemClick);
-            }
+    function onTarefaEditKeyup(event){
+        if(event.which == ENTER_CLICK){
+            salvarTarefaPendente($(this).parent(".tarefa-item"));
         }
     }
 
-    function criarTarefa(tarefa){
-        var $novaTarefa = $("<div />")
-            .addClass("tarefa-item")
-            .append($("<div />")
-                .addClass("tarefa-texto")
-                .text(tarefa))
-            .append($("<div />")
-                .addClass("tarefa-delete"))
-            .append($("<div />")
-                .addClass("clear"));
+    function removerTarefa($removerItem){
+        var $tarefaItem = $removerItem.parent(".tarefa-item");
+
+        $tarefaItem.off("click")
+         $tarefaItem.hide("slow");
+    }
+
+    function focusParaNovaTarefa(){
+        $("#tarefa").val(null).focus();
+    }
+
+    function adicionarTarefa($tarefa){
+        var $novaTarefa = $("<div />").addClass("tarefa-item")
+            .append($("<div />").addClass("tarefa-texto").text($tarefa.val()))
+            .append($("<div />").addClass("tarefa-delete"))
+            .append($("<div />").addClass("clear"));
+
         $("#tarefa-list").append($novaTarefa);
-
-        $("#tarefa").keydown(onTarefaKeyDown);
+        $(".tarefa-delete").click(onTarefaDeleteClick);
         $(".tarefa-item").click(onTarefaItemClick);
-        $(".tarefa-delete").click(onTarefaDelete);
+
+        focusParaNovaTarefa();
     }
 
-    function onTarefaKeyDown(event){
-        if(event.which === 13){
-            var tarefa = $("#tarefa").val();
-            if(tarefa !== null && tarefa !== ""){
-                criarTarefa(tarefa);
-            }
+    function editarTarefa($tarefaEditar){
+       var $inputEdit = $("<input />").val($tarefaEditar.text().trim()).addClass("tarefa-edit");
+        $tarefaEditar.html($inputEdit);
+        $(".tarefa-edit").keyup(onTarefaEditKeyup);
+    }
 
-            $("#tarefa").val(null);
-            $("#tarefa").focus();
+    function onTarefaDeleteClick(){
+       removerTarefa($(this));
+    }
+
+    function onTarefaKeyUp(event){
+        if(event.which === ENTER_CLICK){
+            adicionarTarefa($(this));
         }
     }
 
-    function onTarefaDelete(){
-        $(this).parent(".tarefa-item").hide("slow", function(){
-            $(this).remove();
-        })
+    function salvarTarefaPendente($tarefaPendente){
+        var tarefa = $tarefaPendente.find(".tarefa-edit").val();
 
-        $("#tarefa").val(null);
-        $("#tarefa").focus();
+        $tarefaPendente.empty();
+
+        $tarefaPendente.append($("<div />").addClass("tarefa-text").text(tarefa))
+            .append($("<div />").addClass("tarefa-delete"))
+            .append($("<div />").addClass("clear"));
+
+        $(".tarefa-delete").click(onTarefaDeleteClick);
+        $(".tarefa-item").click(onTarefaItemClick);
     }
 
     function onTarefaItemClick(){
-        if(!$lastClicked){
-        $(this).off();
-        var $tarefaItem = $(this);
-
-        var tarefa = $tarefaItem.find(".tarefa-texto").text();
-
-        $tarefaItem.children().each(function(i, div){
-            $(div).remove();
-        });
-
-        var $editarTarefa = $("<input class='tarefa-edit' type='text' value='" + tarefa + "' />");
-
-        $tarefaItem.append($editarTarefa);
-
-        $tarefaItem.removeClass("tarefa-item");
-        $tarefaItem.addClass("tarefa-edit-item");
-        $lastClicked = true;
-
-        $(".tarefa-edit-item").keydown(onTarefaEditClick);
-        $(".tarefa-edit-item").find(".tarefa-edit").focus();
-    }
+        if(!$(this).is($lastClicked)){
+            if($lastClicked !== undefined){
+                salvarTarefaPendente($lastClicked);
+            }
+            editarTarefa($(this));
+            $lastClicked = $(this);
+            console.log("edit");
+        }
 
     }
 
-    function teste(){
-        var $tarefasItens = $(".tarefa-item").children();
-
-        $tarefasItens.each(function(i, item){
-            $(item).text = "hi";
-        });
-
-    }
-
-
-    $("#tarefa").keydown(onTarefaKeyDown);
-    $(".tarefa-delete").click(onTarefaDelete);
+    $(".tarefa-delete").click(onTarefaDeleteClick);
+    $("#tarefa").keyup(onTarefaKeyUp);
     $(".tarefa-item").click(onTarefaItemClick);
 });
