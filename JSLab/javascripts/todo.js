5ab6aa1f506f475fbc745baf78e647e23f8146b4
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2014-01-20 12:11:14 -0200

    chapter 8, integranting app with heroku server example.

--- JSLab/javascripts/todo.js
+++ JSLab/javascripts/todo.js
@@ -1,6 +1,13 @@
 $(function(){
     const ENTER_CLICK = 13;
+    var meu_login = "seu@email.com";
+    var server = "http://livro-capitulo07.herokuapp.com";
     var $lastClicked;
+
+    function updateTarefa(tarefaTexto, id){
+        var params = {tarefa_id: id, texto: tarefaTexto};
+        $.post(server + "/tarefa", params);
+    }
 
     function onTarefaEditKeyup(event){
         if(event.which == ENTER_CLICK){
@@ -10,31 +17,47 @@ $(function(){
 
     function removerTarefa($removerItem){
         var $tarefaItem = $removerItem.parent(".tarefa-item");
-
-        $tarefaItem.off("click")
-         $tarefaItem.hide("slow");
+        $tarefaItem.off("click");
+        var params = {usuario: meu_login, tarefa_id: $tarefaItem.find(".tarefa-id").text(), _method: "DELETE"};
+        $.post(server + "/tarefa", params, function(data){
+            $tarefaItem.hide("slow");
+        });
     }
 
     function focusParaNovaTarefa(){
         $("#tarefa").val(null).focus();
     }
 
-    function adicionarTarefa($tarefa){
+    function salvarNovaTarefa($novaTarefa){
+        var params = {usuario: meu_login, _method: "PUT", texto: $novaTarefa.find(".tarefa-texto").text()};
+        $.post(server + "/tarefa", params, function(data){
+            $novaTarefa.find(".tarefa-id").text(data.id);
+        });
+    }
+
+    function adicionarTarefa(tarefa, id){
+        id = id || 0;
         var $novaTarefa = $("<div />").addClass("tarefa-item")
-            .append($("<div />").addClass("tarefa-texto").text($tarefa.val()))
+            .append($("<div />").addClass("tarefa-id").text(id))
+            .append($("<div />").addClass("tarefa-texto").text(tarefa))
             .append($("<div />").addClass("tarefa-delete"))
             .append($("<div />").addClass("clear"));
-
         $("#tarefa-list").append($novaTarefa);
         $(".tarefa-delete").click(onTarefaDeleteClick);
         $(".tarefa-item").click(onTarefaItemClick);
-
+        if(id === 0){
+            salvarNovaTarefa($novaTarefa);
+        }
         focusParaNovaTarefa();
     }
 
     function editarTarefa($tarefaEditar){
-       var $inputEdit = $("<input />").val($tarefaEditar.text().trim()).addClass("tarefa-edit");
-        $tarefaEditar.html($inputEdit);
+       var $inputEdit = $("<input />").val($tarefaEditar.find(".tarefa-texto").text().trim()).addClass("tarefa-edit");
+        var $tarefaId = $("<div />").addClass("tarefa-id").text($tarefaEditar.find(".tarefa-id").text().trim());
+        $tarefaEditar.empty();
+        $tarefaEditar.append($tarefaId)
+            .append($inputEdit);
+        $tarefaEditar.focus();
         $(".tarefa-edit").keyup(onTarefaEditKeyup);
     }
 
@@ -43,20 +66,20 @@ $(function(){
     }
 
     function onTarefaKeyUp(event){
-        if(event.which === ENTER_CLICK){
-            adicionarTarefa($(this));
+        if(event.which === ENTER_CLICK && $(this).val() !== ""){
+            adicionarTarefa($(this).val());
         }
     }
 
     function salvarTarefaPendente($tarefaPendente){
         var tarefa = $tarefaPendente.find(".tarefa-edit").val();
-
+        var tarefaId = $tarefaPendente.find(".tarefa-id").text();
         $tarefaPendente.empty();
-
-        $tarefaPendente.append($("<div />").addClass("tarefa-text").text(tarefa))
+        $tarefaPendente.append($("<div />").addClass("tarefa-id").text(tarefaId))
+            .append($("<div />").addClass("tarefa-texto").text(tarefa))
             .append($("<div />").addClass("tarefa-delete"))
             .append($("<div />").addClass("clear"));
-
+        updateTarefa(tarefa, tarefaId);
         $(".tarefa-delete").click(onTarefaDeleteClick);
         $(".tarefa-item").click(onTarefaItemClick);
     }
@@ -68,12 +91,21 @@ $(function(){
             }
             editarTarefa($(this));
             $lastClicked = $(this);
-            console.log("edit");
         }
-
     }
 
-    $(".tarefa-delete").click(onTarefaDeleteClick);
+    function carregarTarefas(){
+        $("#tarefa").empty();
+        var params = {usuario: meu_login};
+        $.getJSON(server + "/tarefas", params).done(function(data){
+            console.log("data: ", data);
+            for(var tarefa = 0; tarefa < data.length; tarefa++) {
+                adicionarTarefa(data[tarefa].texto, data[tarefa].id);
+            }
+        });
+    }
+
+    carregarTarefas();
     $("#tarefa").keyup(onTarefaKeyUp);
-    $(".tarefa-item").click(onTarefaItemClick);
+
 });
